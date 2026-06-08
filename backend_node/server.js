1704bbc2d04c91c624b45a928b6d675a9de3a4b6
Aryan Goel <aryangoel984@gmail.com>
2026-06-08 19:13:18 +0530

    Configure Vercel deployment parameters

--- backend_node/server.js
+++ backend_node/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const prisma = new PrismaClient();
 
 app.use(cors());
